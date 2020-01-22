8562d0fa7d756893dcf17de2148b1b8f74184dc8
williamjiamin <lxodgf@163.com>
2020-01-22 21:51:50 +0800

    init commit

--- 2.Big_O_Example.py
+++ 2.Big_O_Example.py
@@ -1,10 +1,24 @@
-some_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+# some_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+#
+#
+# def func_nested_for_loop(some_list):
+#     for elem_1 in some_list:
+#         for elem_2 in some_list:
+#             print(elem_1, elem_2)
+#
+#
+# func_nested_for_loop(some_list)
+
+some_list_02 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
 
-def func_nested_for_loop(some_list):
-    for elem_1 in some_list:
-        for elem_2 in some_list:
-            print(elem_1, elem_2)
+def find_the_match(search_list, match_elem):
+    for elem in search_list:
+        if elem == match_elem:
+            return print("Find the match")
+    return print("Did not find it")
 
+#
+# find_the_match(some_list_02, 1)
 
-func_nested_for_loop(some_list)
+find_the_match(some_list_02, 20)
