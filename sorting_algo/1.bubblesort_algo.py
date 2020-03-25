bbcc696b62cc609eddbb6710beb78f99dbc4fe08
williamjiamin <lxodgf@163.com>
2020-03-25 23:22:43 +0800

    add bubblesort

---
+++ sorting_algo/1.bubblesort_algo.py
@@ -0,0 +1,31 @@
+def bubble_sort_algo(list_to_be_sorted):
+    for i in range(0, len(list_to_be_sorted) - 1):
+        for j in range(0, len(list_to_be_sorted) - 1 - i):
+            if list_to_be_sorted[j] > list_to_be_sorted[j + 1]:
+                list_to_be_sorted[j], list_to_be_sorted[j + 1] = list_to_be_sorted[j + 1], list_to_be_sorted[j]
+                print(list_to_be_sorted)
+    return list_to_be_sorted
+
+
+cust_list = [3, 2, 4, 1, 5, 6, 7, 8, 9, 10, 2, 3, 6, 6]
+
+
+# alpha_list = ["d", "a", "b", "c"]
+# print(bubble_sort_algo(cust_list))
+# bubble_sort_algo(cust_list)
+# bubble_sort_algo(alpha_list)
+
+def bubble_sort_algo_v2(list_to_be_sorted):
+    for i in range(0, len(list_to_be_sorted) - 1):
+        # check whether there is a swap going on
+        no_swap_going_on = True
+        for j in range(0, len(list_to_be_sorted) - 1 - i):
+            if list_to_be_sorted[j] > list_to_be_sorted[j + 1]:
+                list_to_be_sorted[j], list_to_be_sorted[j + 1] = list_to_be_sorted[j + 1], list_to_be_sorted[j]
+                no_swap_going_on = False
+        if no_swap_going_on == True:
+            break
+    return list_to_be_sorted
+
+
+print(bubble_sort_algo_v2(cust_list))
